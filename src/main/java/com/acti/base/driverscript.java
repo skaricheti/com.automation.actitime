438f0fcdc9fd29b2c011950be1554514ec5a2247
mkari <mkari@LAPTOP-MQEROLNH>
2022-03-24 18:27:57 -0500

    initial automation framework

---
+++ src/main/java/com/acti/base/driverscript.java
@@ -0,0 +1,16 @@
+package com.acti.base;
+
+import org.openqa.selenium.WebDriver;
+
+public class driverscript {
+	
+	public static WebDriver driver;
+	
+	public abstract driverscript();
+	
+	
+	
+		
+	}
+
+}
